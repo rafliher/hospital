82544fadad47ab6f01e6740de541c2a3bdbf1904
rafliher <55349616+rafliher@users.noreply.github.com>
2021-08-02 11:09:44 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "hospitaldb",
+  HOST: "ec2-18-235-109-97.compute-1.amazonaws.com:5432",
+  USER: "pjutebxgtqoacn",
+  PASSWORD: "a96074bba3ad6f4d151290c834b5523e548050ff159716f1d6d11a30b5a1ec30",
+  DB: "d2umv9q3jpr9th",
   dialect: "mysql",
   pool: {
     max: 5,
